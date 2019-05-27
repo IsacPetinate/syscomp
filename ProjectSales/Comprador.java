dd42ceac069471af71effad04c8823546305c89b
Isac Petinate <idpetinate@gmail.com>
2019-05-26 23:16:26 -0300

    Criando método de cadastro de produto

--- ProjectSales/Comprador.java
+++ ProjectSales/Comprador.java
@@ -4,29 +4,39 @@ import javax.swing.JOptionPane;
 
 public class Comprador
 {
+    private Object produto;
 
-    public void comprarProduto ()
-    {
+    //
+    public Object getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Object produto) {
+        this.produto = produto;
+    }
+    //
+
+    public void comprarProduto() {
         int valor = 1000;
 
         double preco = Integer.parseInt(JOptionPane.showInputDialog("Informe o valor do produto "));
         double qtd = Integer.parseInt(JOptionPane.showInputDialog("Informe a quantidade"));
 
-        //calcula quanto o usuario gastou
+        // calcula quanto o usuario gastou
         double resultado = preco * qtd;
 
-        //calcula o saldo restante
-        double divida = valor - resultado ;
+        // calcula o saldo restante
+        double divida = valor - resultado;
 
-        //exibe o valor gasto
-        JOptionPane.showMessageDialog(null,"Valor Gasto: "+resultado,"Resultado",JOptionPane.INFORMATION_MESSAGE);
+        // exibe o valor gasto
+        JOptionPane.showMessageDialog(null, "Valor Gasto: " + resultado, "Resultado", JOptionPane.INFORMATION_MESSAGE);
 
-        //exibe o saldo
-        JOptionPane.showMessageDialog(null,"Saldo Restante: "+divida,"Resultado",JOptionPane.INFORMATION_MESSAGE);
+        // exibe o saldo
+        JOptionPane.showMessageDialog(null, "Saldo Restante: " + divida, "Resultado", JOptionPane.INFORMATION_MESSAGE);
+    }
 
-
-
-
-
+    public void cadastrarProduto(String nome, String codigo, int quantidade, double valor)
+    {
+        // Cadastro vai aqui
     }
 }
--- ProjectSales/Vendedor.java
+++ ProjectSales/Vendedor.java
@@ -1,6 +1,6 @@
 package ProjectSales;
 
-import java.util.ArrayList;
+// import java.util.ArrayList;
 
 public class Vendedor
 {
