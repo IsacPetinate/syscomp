20d65256c17cbabb22b7865a2727ef0ee37b8d7e
Isac Petinate <idpetinate@gmail.com>
2019-05-26 23:19:21 -0300

    Limpando espaços e outros na classe Main

--- ProjectSales/Main.java
+++ ProjectSales/Main.java
@@ -5,43 +5,34 @@ import java.util.Scanner;
 
 public class Main
 {
-    public static void main (String args[])
+    public static void main(String args[])
     {
-        Scanner entrada = new Scanner (System.in);
+        Scanner entrada = new Scanner(System.in);
 
-        Object[] opcoesPossiveis = {"Vendedor","Comprador"};
+        Object[] opcoesPossiveis = { "Vendedor", "Comprador" };
 
         int selecionado;
 
-        Object botaoSelecionado = JOptionPane.showOptionDialog(null,"Escolha:",
-                "Opções",
-                JOptionPane.DEFAULT_OPTION,
-                JOptionPane.INFORMATION_MESSAGE,
-                null, opcoesPossiveis,
-                opcoesPossiveis[0]);
+        Object botaoSelecionado = JOptionPane.showOptionDialog(null, "Escolha:", "Opções",
+            JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, 
+            null, opcoesPossiveis, opcoesPossiveis[0]
+        );
 
-        //convertendo para string
-        selecionado= Integer.parseInt(botaoSelecionado.toString());
+        // convertendo para string
+        selecionado = Integer.parseInt(botaoSelecionado.toString());
 
-
-
-
-        if(selecionado == 1)
-        {
+        if (selecionado == 1) {
 
             Comprador obj = new Comprador();
             obj.comprarProduto();
 
-            //if necessario para que o usuario nao gaste mais que o permitido
-            //if()
-
+            // if necessario para que o usuario nao gaste mais que o permitido
+            // if()
         }
 
-
-
-        //Serve para fechar a entrada de dados para nao dar erro no code, é considerado boas praticas também
+        // Serve para fechar a entrada de dados para nao dar erro no code, é considerado
+        // boas praticas também
         entrada.close();
     }
-
 
 }
