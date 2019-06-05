7fd44a71fbbfd05645a9cab37c239b9f1a7358bd
Isac Petinate <idpetinate@gmail.com>
2019-06-04 23:33:34 -0300

    Correções no local do arquivo

--- ProjectSales/Janela.java
+++ ProjectSales/Janela.java
@@ -1,14 +1,18 @@
-package ProjectSales.GUI;
+package ProjectSales;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Janela {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Syscomp");
-        JLabel label = new JLabel("tituloLabel");
-        JPanel panel = new JPanel();
+        String nomeBotao = "OK";
+
+        JFrame  frame  = new JFrame("Syscomp");
+        JLabel  label  = new JLabel("tituloLabel");
+        JPanel  panel  = new JPanel();
+        JButton button = new JButton(nomeBotao);
 
         frame.setVisible(true);
         frame.setSize(500, 500);
@@ -16,6 +20,7 @@ public class Janela {
 
         frame.add(label);
         frame.add(panel);
+        frame.add(button);
     }
 
     public void criaJanela() {
