1322f18d9a27cd1a7e325c1110556bbe73954f41
Isac Petinate <idpetinate@gmail.com>
2019-06-04 23:29:19 -0300

    Implementando Frame

--- ProjectSales/GUI/Janela.java
+++ ProjectSales/GUI/Janela.java
@@ -1,12 +1,24 @@
 package ProjectSales.GUI;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class Janela {
     public static void main(String[] args) {
+    
+    }
+
+    public void criaJanela(String tituloLabel) {
         JFrame frame = new JFrame("Syscomp");
+        JLabel label = new JLabel(tituloLabel);
+        JPanel panel = new JPanel();
+
         frame.setVisible(true);
         frame.setSize(500, 500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.add(label);
+        frame.add(panel);
     }
 }
