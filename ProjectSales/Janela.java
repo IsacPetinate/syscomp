9dccb7c2e15347eef2b27f059135b5f639218654
Isac Petinate <idpetinate@gmail.com>
2019-06-04 23:40:15 -0300

    Correções no Panel e Button

--- ProjectSales/Janela.java
+++ ProjectSales/Janela.java
@@ -1,5 +1,8 @@
 package ProjectSales;
 
+import java.awt.Dimension;
+import java.awt.GridLayout;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -11,15 +14,18 @@ public class Janela {
 
         JFrame  frame  = new JFrame("Syscomp");
         JLabel  label  = new JLabel("tituloLabel");
-        JPanel  panel  = new JPanel();
+        JPanel  panel  = new JPanel(new GridLayout(4,4,4,4));
         JButton button = new JButton(nomeBotao);
 
+        // Configurações do Frame
         frame.setVisible(true);
         frame.setSize(500, 500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+        // Confgurações do Botao
+        button.setSize(30, 30);
+        // Adição de elementos no frame
+        frame.setContentPane(panel);
         frame.add(label);
-        frame.add(panel);
         frame.add(button);
     }
 
